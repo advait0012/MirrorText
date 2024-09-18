2b22a80e864ec59d18e1266362379d369c1e836a
advait0012 <advait0012@proton.me>
2024-09-18 21:27:10 +0000

    reversing the text done

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-// const inputEl = document.querySelector("input");
-// const butonEl = document.querySelector("[btninput]");
-// const chipEl = document.querySelector(".chip");
-
-// inputEl.addEventListener("keypress", (event) => {
-//   if (event.keyCode === 13) {
-//     let inputVal = inputEl.value;
-//     let mirrorText = inputVal.split("").reverse().join("");
-//     console.log(mirrorText);
-//   }
-// });
-
 const inputElement = document.querySelector("input");
 const buttonElement = document.querySelector("button")
 const copied = document.querySelector(".chip")
