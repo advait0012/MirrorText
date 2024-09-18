943e0ea435e88a369a7d80f13e189fdfe87f86e0
advait0012 <advait0012@proton.me>
2024-09-18 21:26:07 +0000

    reversing done

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-const inputEl = document.querySelector("input")
-const butonEl = document.querySelector("[btninput]")
-const chipEl = document.querySelector(".chip")
+// const inputEl = document.querySelector("input");
+// const butonEl = document.querySelector("[btninput]");
+// const chipEl = document.querySelector(".chip");
 
+// inputEl.addEventListener("keypress", (event) => {
+//   if (event.keyCode === 13) {
+//     let inputVal = inputEl.value;
+//     let mirrorText = inputVal.split("").reverse().join("");
+//     console.log(mirrorText);
+//   }
+// });
 
-const btn = document.querySelector('[btninput]');
-btn.addEventListener("click", () => {
-  alert("Hello");
-});
+const inputElement = document.querySelector("input");
+const buttonElement = document.querySelector("button")
+const copied = document.querySelector(".chip")
 
-
-inputEl.addEventListener("keypress",(e)=>{
-  console.log(e.key);
-  
+inputElement.addEventListener("keypress",(e)=>{
+    if(e.keyCode === 13){
+        let inputVal = inputElement.value;
+        let mirrorText = inputVal.split("").reverse().join("");
+        inputElement.value = mirrorText;
+    }
 })
