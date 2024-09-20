ec461689a24bba7e584c39a006f9e961f5c3834f
advait0012 <advait0012@proton.me>
2024-09-20 17:15:07 +0000

    UI almost done

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
 const themeToggle = document.querySelector(".theme")
-const bodyElement = document.body;
-const inputElement = document.querySelector("input");
-const buttonElement = document.querySelector("button")
+const inputElement = document.querySelector("[data-input]");
+const buttonElement = document.querySelector("b")
 const copied = document.querySelector(".chip")
 
-inputElement.addEventListener("keypress",(e)=>{
-    if(e.keyCode === 13){
-        let inputVal = inputElement.value;
-        let mirrorText = inputVal.split("").reverse().join("");
-        inputElement.value = mirrorText;
-    }
-})
+// inputElement.addEventListener("keypress",(e)=>{
+//     if(e.keyCode === 13){
+//         let inputVal = inputElement.value;
+//         let mirrorText = inputVal.split("").reverse().join("");
+//         inputElement.value = mirrorText;
+//     }
+// })
 
-themeToggle.addEventListener("click",()=>{
-    bodyElement.classList.toggle("night")
-})
